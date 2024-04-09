cebcf1865c46bfa14d501ad03c5118f63a0d1c11
Dmitiy Demidyuk work <djeemy2014@gmail.com>
2024-04-09 13:05:27 +0300

    server

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const optionsPath={ root: __dirname }
+
+const app=express()
+const port = 8080
+const host = '127.0.0.1'
+
+app.get('/',function(req,res) {
+    res.sendFile('./index.html',optionsPath);
+    console.log('/', req.ip,  new Date())
+    //console.log(req)
+  });
+
+app.listen(port, () => {
+    console.log(`listening on host ${host} port ${port}`)
+    console.log(`http://${host}:${port}/`)
+  
+  })
+
